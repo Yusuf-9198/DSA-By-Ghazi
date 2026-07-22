084568bd4984bc75271441888f2a7272f3e139f6
Mohd Yusuf Beig <93648991+Yusuf-9198@users.noreply.github.com>
2026-07-22 19:37:25 +0530

    Time: 79 ms (72%), Space: 48.1 MB (53.33%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -1,24 +1,56 @@
-//correct
-import java.util.ArrayList;
+// //correct   tc= O(n) , sc = O(n)
+// import java.util.ArrayList;
+// class Solution {
+//     public int maxActiveSectionsAfterTrade(String s) {
+//         int ones = 0, n = s.length();
+//         List<Integer> zerosBlockSize = new ArrayList<>();
+//         for (int i = 0; i < n; ) {
+//             int j = i;
+//             while (j < n && s.charAt(j) == s.charAt(i)) j++;
+//             int len = j - i;
+//             if (s.charAt(i) == '1') {
+//                 ones += len;
+//             } else {
+//                 zerosBlockSize.add(len);
+//             }
+//             i = j;
+//         }
+//         int maxZeroSum = 0;
+//         for (int i = 0; i < zerosBlockSize.size() - 1; i++) {
+//             maxZeroSum = Math.max(maxZeroSum, zerosBlockSize.get(i) + zerosBlockSize.get(i + 1));
+//         }
+//         return ones + maxZeroSum;
+//     }
+// }
+
+
+// other way  tc= O(n) , sc = O(1)
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
-        int ones = 0, n = s.length();
-        List<Integer> zeros = new ArrayList<>();
-        for (int i = 0; i < n; ) {
+        int totalOnes = 0;
+        int maxZeroSum = 0;
+        int prevZeroLen = 0; // Tracks the length of the previous '0' segment
+        
+        int i = 0, n = s.length();
+        while (i < n) {
             int j = i;
-            while (j < n && s.charAt(j) == s.charAt(i)) j++;
+            while (j < n && s.charAt(j) == s.charAt(i)) {
+                j++;
+            }
             int len = j - i;
+            
             if (s.charAt(i) == '1') {
-                ones += len;
+                totalOnes += len;
             } else {
-                zeros.add(len);
+                // If we have seen at least one zero segment prior, consider merging
+                if (prevZeroLen > 0) {
+                    maxZeroSum = Math.max(maxZeroSum, prevZeroLen + len);
+                }
+                prevZeroLen = len;
             }
             i = j;
         }
-        int maxZeroSum = 0;
-        for (int i = 0; i < zeros.size() - 1; i++) {
-            maxZeroSum = Math.max(maxZeroSum, zeros.get(i) + zeros.get(i + 1));
-        }
-        return ones + maxZeroSum;
+        
+        return totalOnes + maxZeroSum;
     }
 }
